74c9edca07ae9c8d11231eedf63660acdca3e0d6
phamthang98 <shippercoder@gmail.com>
2023-04-19 15:50:35 +0700

    add get prive

--- Crawl.py
+++ Crawl.py
@@ -20,9 +20,25 @@ links = [elem.get_attribute('href') for elem in elems]
 
 #Get Price
 elems_price = driver.find_elements(By.CSS.SELECTOR , "aBrP0")
-elems_price = [elem_price.text for elem_price in elems_price]
+price = [elem_price.text for elem_price in elems_price]
 
 df1 = pd.DataFrame(list(zip(title, price, links)), columns = ['title', 'price', 'link_item'])
 df1['index_'] = np.arange(1, len(df1) + 1)
+
+#Get Discount
+elems_discount = driver.find_elements(By.CSS_SELECTOR , "WNoq3")
+discount_all = [elem.text for elem in elems_discount]
+#elems_discount = driver.find_elements(By.CSS_SELECTOR , "WNoq3 ._1m41m")
+#discount = [elem.text for elem in elems_discount]
+
+#elems_discountPercent = driver.find_elements(By.CSS_SELECTOR , "WNoq3 .IcOsH")
+#discountPercent = [elem.text for elem in elems_discountPercent]
+
+#elems_discountText = driver.find_elements(By.CSS_SELECTOR , "WNoq3 .ic-dynamic-badge ic-dynamic-badge-text ic-dynamic-badge-fc ic-dynamic-group-2")
+#discountText = [elem.text for elem in elems_discountText]
+
+
+df2 = pd.DataFrame(list(zip(discount_all)), columns=['discount_all'])
+df2['discount_idx'] = np.arange(1, len(df2) +1)
 
 df1.head()
